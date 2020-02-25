542009e2763628714e9f75a01353ab8d174762c5
DeVil2O <chrggrg2018@gmail.com>
2020-02-25 14:40:54 +0530

    further improvements and changes are in comments

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from trav_directory import *
 username = 'devi'
 
 
+# Comments
+#   - make traversal for root directories
+#   - make root and superuser account and name other than root
+#   - also show username and password
+#   - make another file and remove the stuff from this file
+#   - this should be home file for superuser account
+
 def just_traverse_directory():
     path = input('Enter the directory name: ')
     traversing_func('/home/'+username+'/'+path)
@@ -10,3 +17,6 @@ def just_traverse_directory():
 
 def list_directories_devi():
     traversing_func('/home/'+username)
+
+
+def
