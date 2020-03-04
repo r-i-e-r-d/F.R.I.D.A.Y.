f5bf205226a9c372c2b1082c71c1b043136939df
DeVil2O <chrggrg2018@gmail.com>
2020-03-04 18:47:58 +0530

    Modified the authorization.py

--- authorization.py
+++ authorization.py
@@ -1,4 +1,38 @@
-# import getpass_ak
+import getch
+import sys
 
-# password = getpass_ak.getpass("Auth Key : ")
-# while(1):
+
+def userName():
+    username = input("Enter The Username : ")
+    return username
+
+
+def password():
+    print("Enter the Password : ")
+    passString = ""
+    while True:
+        c = getch.getch()
+        if c == "\n":
+            return passString
+        elif ord(c) == 127:
+            if len(passString) > 0:
+                sys.stdout.write("\x08 \x08")
+                sys.stdout.flush()
+                passString = passString[0:-1]
+            continue
+        else:
+            sys.stdout.write("*")
+            sys.stdout.flush()
+            passString += c
+
+
+if __name__ == "__main__":
+    print("************  WANT TO BE NEW WORLD  ************ : (Y/n)")
+    choice = getch.getch()
+    if choice == "y" or choice == "Y":
+        username = userName()
+        while len(username) == 0:
+            username = userName()
+        password = password()
+    else:
+        print("\n\n************  YES YOU DON'T DESERVE THIS WORLD  ************")
